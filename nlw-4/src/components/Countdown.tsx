69711fefb28f973dd5f03fef45d37fabfa46c366
André Borges <hoilut@hotmail.com>
2021-03-03 21:22:52 -0300

    day-4

--- nlw-4/src/components/Countdown.tsx
+++ nlw-4/src/components/Countdown.tsx
@@ -2,45 +2,21 @@ import { useState, useEffect, useContext } from 'react';
 import { start } from 'repl';
 import { clearTimeout } from 'timers';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/Countdown.module.css';
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-  const { startNewChallenge } = useContext(ChallengesContext);
-  const [time, setTime] = useState(25 * 0.2);
-  const [isActive, setIsActive] = useState(false);
-  const [hasFinished, setHasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
+  const {
+    minutes,
+    seconds,
+    hasFinished,
+    isActive,
+    startCountdown,
+    resetCountdown,
+  } = useContext(CountdownContext);
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-  function startCountdown() {
-    setIsActive(true);
-  }
-
-  function resetCountdown() {
-    window.clearTimeout(countdownTimeout); // Cancela o setTimeout
-    setIsActive(false);
-    setTime(25 * 60);
-  }
-
-  // 1º parametro função, 2º parametro variavel
-  // Executa quando a variavel muda de valor [isActive, time]
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else if (isActive && time === 0) {
-      setHasFinished(true);
-      setIsActive(false);
-      startNewChallenge();
-    }
-  }, [isActive, time]);
 
   return (
     <div>
