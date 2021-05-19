114b86001b798c9218aebf922f2b2695d8bdc9cd
André Borges <hoilut@hotmail.com>
2021-05-19 01:43:35 -0300

    day-3

--- login-form/src/pages/Login/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UserLogin from '../../components/Login/Login';
-
-const PagesLogin = () => (
-  <UserLogin />
-);
-
-export default PagesLogin;
--- nlw-5/src/pages/index.tsx
+++ nlw-5/src/pages/index.tsx
@@ -1,19 +1,73 @@
-// SPA
-// SSR
-// SSG
+// SPA, SSR, SSG
 
-export default function Home(props) {
-  return <h1></h1>;
+import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+import styles from './home.module.scss';
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  published_at: string;
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
+  return (
+    <div className={styles.homepage}>
+      <section className={styles.latesEpisodes}>
+        <h2>Últimos lançamentos</h2>
+        <ul></ul>
+      </section>
+      <section className={styles.allEpisodes}></section>
+    </div>
+  );
 }
 
-export async function getServerSideProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+// getStaticSideProps do next
+export const getStaticSideProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes?', {
+    params: {
+      _limit: 12,
+      _sorte: 'published_at',
+      _order: 'desc',
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
 
   return {
     props: {
-      episodes: data,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
